53076898fe701957bc242e1cc9755db2d5bcbc3c
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-20 21:18:59 +0100

    Added styling for personal section

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -1,35 +1,36 @@
 import React from 'react'
+import './personal.css'
 
 function Personal() {
     return (
         <div className='personal flex column'>
-            <div className='fullname flex column'>
+            <div className='name flex column'>
                 <span className='firstname'>First Name</span>
                 <span className='lastname'>Last Name</span>
             </div>
             <div className='photo-container'>
-                <img src='#' alt='placeholder'></img>
+                <img src='#' alt=''></img>
             </div>
-            <div className='contact'>
+            <div className='contact section'>
                 <h2>Contact</h2>
-                <div className='address'>
+                <div className='address category'>
                     <div className='category-name'>Adress:</div>
                     <div className='value'>Placeholder</div>
                 </div>
-                <div className='phone'>
+                <div className='phone category'>
                     <div className='category-name'>Phone:</div>
                     <div className='value'>Placeholder</div>
                 </div>
-                <div className='email'>
+                <div className='email category'>
                     <div className='category-name'>email:</div>
                     <div className='value'>Placeholder</div>
                 </div>
-                <div className='linkedin'>
+                <div className='linkedin category'>
                     <div className='category-name'>LinkedIn:</div>
                     <div className='value'>Placeholder</div>
                 </div>
             </div>
-            <div className='skills'>
+            <div className='skills section'>
                 <h2>Skills</h2>
                 <div className='list'>
                     <ul>
@@ -39,11 +40,12 @@ function Personal() {
                     </ul>
                 </div>
             </div>
-            <div className='languages'>
+            <div className='languages section'>
                 <h2>Languages</h2>
                 <div id='languages-list'>
                     <div class='language-container'>
-                        <span className='language-name'>English</span><span className='language-proficiency'>Proficient</span>
+                        <div className='name'>English</div>
+                        <div className='proficiency'>(Limited working proficiency)</div>
                     </div>
                 </div>
             </div>
