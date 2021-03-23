cfe096a283fc2919fee1e881a2117292b0167068
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-23 18:46:25 +0100

    Restyled name and contact input fields

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import Professional from './Components/Professional'
 class App extends React.Component {
   constructor() {
     super()
-    this.addEntry = this.addEntry.bind(this)
+    this.submitForm = this.submitForm.bind(this)
     this.handleChange = this.handleChange.bind(this)
+
+    this.personal = {
+      firstname: '',
+      lastname: ''
+    }
 
     this.state = {
       firstname: 'John',
@@ -20,15 +25,23 @@ class App extends React.Component {
     }
   }
 
-  addEntry(e) {
+  submitForm(e) {
+    console.log('Form Submitted', this.personal)
     e.preventDefault();
+
+    const {firstname, lastname, adress, phone, email, linkedIn} = this.personal
+    this.setState({
+      firstname: firstname,
+      lastname: lastname,
+      adress: adress,
+      phone: phone,
+      email: email,
+      linkedIn: linkedIn
+    })
   }
 
   handleChange(e) {
-    console.log(e.target.name)
-    this.setState({
-      [e.target.name]: e.target.value
-    })
+    this.personal[e.target.name] = e.target.value
   }
 
   render() {
@@ -37,13 +50,20 @@ class App extends React.Component {
         <div className='cv-form'>
           <div className='form-section'>
             <h2>Personal information</h2>
-            <form className='flex column' onSubmit={this.addEntry}>
-              <input name='firstname' className='personal-input' placeholder='First name' onChange={this.handleChange}></input>
-              <input name='lastname' className='personal-input' placeholder='Last name' onChange={this.handleChange}></input>
-              <input name='adress' className='personal-input' placeholder='Adress' onChange={this.handleChange}></input>
-              <input name='phone' className='personal-input' placeholder='Phone' onChange={this.handleChange}></input>
-              <input name='email' className='personal-input' placeholder='Email' onChange={this.handleChange}></input>
-              <input name='linkedIn' className='personal-input' placeholder='LinkedIn' onChange={this.handleChange}></input>
+            <form className='flex column' onSubmit={this.submitForm}>
+              <div className ='flex justified-center'>
+                <div className ='flex column' style={{flex: 1}}>
+                  <input name='firstname' className='personal-input' placeholder='First name' onChange={this.handleChange}></input>
+                  <input name='lastname' className='personal-input' placeholder='Last name' onChange={this.handleChange}></input>
+                  <input name='adress' className='personal-input' placeholder='Adress' onChange={this.handleChange}></input>
+                </div>
+                <div className='flex column' style={{flex: 1}}>
+                  <input name='phone' className='personal-input' placeholder='Phone' onChange={this.handleChange}></input>
+                  <input name='email' className='personal-input' placeholder='Email' onChange={this.handleChange}></input>
+                  <input name='linkedIn' className='personal-input' placeholder='LinkedIn' onChange={this.handleChange}></input>
+                </div>
+              </div>
+              <button type='submit'>Submit</button>
             </form>
           </div>
           
