dc2fc010bccaa590dd112e089daab5182792af8c
Danijel Stanic <danijel.stanich@gmail.com>
2021-03-23 21:10:13 +0100

    Moved form section into separate component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 
+import Form from './Components/Form'
 import Personal from './Components/Personal'
 import Professional from './Components/Professional'
 
@@ -62,39 +63,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='flex justify-center scale-down'>
-        <div className='cv-form'>
-          <div className='form-section'>
-            <h2>Personal information</h2>
-            <form className='flex column' onSubmit={this.submitForm}>
-              <div className ='flex justified-center'>
-                <div className ='flex column' style={{flex: 1}}>
-                  <input name='firstname' className='personal-input' placeholder='First name' onChange={this.handleChange}></input>
-                  <input name='lastname' className='personal-input' placeholder='Last name' onChange={this.handleChange}></input>
-                  <input name='adress' className='personal-input' placeholder='Adress' onChange={this.handleChange}></input>
-                </div>
-                <div className='flex column' style={{flex: 1}}>
-                  <input name='phone' className='personal-input' placeholder='Phone' onChange={this.handleChange}></input>
-                  <input name='email' className='personal-input' placeholder='Email' onChange={this.handleChange}></input>
-                  <input name='linkedIn' className='personal-input' placeholder='LinkedIn' onChange={this.handleChange}></input>
-                </div>
-              </div>
-              <button type='submit'>Submit</button>
-            </form>
-          </div>
-          <div className='skills form-section'>
-            <h2>Skills</h2>
-            <div className=''>
-              <form onSubmit={this.addSkill}>
-                <input name='skill'></input>
-                <button type='submit'>Add Skill</button>
-              </form>
-            </div>
-           
 
-          </div>
-  
-        </div>
+      <div className='flex justify-center scale-down'>
+        <Form submitForm={this.submitForm} handleChange={this.handleChange} addSkill={this.addSkill}/>
         <div className='cv flex'>
           <Personal data={this.state}/>
           <Professional />
