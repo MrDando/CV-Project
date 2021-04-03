b63c8f36ee200995cd991eaafe984a23681221b2
Danijel Stanic <danijel.stanich@gmail.com>
2021-04-03 14:28:37 +0200

    Fixed Header styling issues

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import uniqid from "uniqid";
 
+import './Header.css'
+
 function Options(props) {
     return (
         props.colorSchemes.map(scheme => {
@@ -16,24 +18,26 @@ function Header (props) {
           <div className='webpage-title'>
             <h1>CV Creator</h1>
           </div>
-          <div className='select-container flex'>
-            <div>
-              <p>CV Template</p>
-              <select className='cv-select select-css' onChange={props.switchCV}>
-                      <option value="1">Template1</option>
-                      <option value="2">Template2</option>
-                      <option value="3">Template3</option>
-              </select>
-            </div>
-            <div style={{marginLeft: '20px'}}>
-                <p>Color Scheme</p>
-                <select className='colorscheme-select select-css' onChange={props.switchColorScheme}>
-                    <Options colorSchemes={props.colorSchemes}/>
+          <div className='header-right flex align-center'>
+            <div className='select-container flex'>
+              <div>
+                <p>CV Template</p>
+                <select className='cv-select select-css' onChange={props.switchCV}>
+                        <option value="1">Template1</option>
+                        <option value="2">Template2</option>
+                        <option value="3">Template3</option>
                 </select>
+              </div>
+              <div style={{marginLeft: '20px'}}>
+                  <p>Color Scheme</p>
+                  <select className='colorscheme-select select-css' onChange={props.switchColorScheme}>
+                      <Options colorSchemes={props.colorSchemes}/>
+                  </select>
+              </div>
             </div>
-          </div>
-          <div className='github-mark-container'>
-            <a href='https://github.com/MrDando/CV-Project' target="_blank" rel="noreferrer"><img src={props.gitHubURL} alt=''></img></a>
+            <div className='github-mark-container'>
+              <a href='https://github.com/MrDando/CV-Project' target="_blank" rel="noreferrer"><img src={props.gitHubURL} alt=''></img></a>
+            </div>
           </div>
         </header>
     )
